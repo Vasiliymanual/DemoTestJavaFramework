dc7a99489d70bc9ead8dd59c5a3f41149c9622c7
Vasiliymanual <121040936+Vasiliymanual@users.noreply.github.com>
2023-05-27 00:30:20 +0400

    Initial commit broken

---
+++ src/test/java/AttemptAtCreatingTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class AttemptAtCreatingTest {
+    @Test
+    public void SuccessfulTest() {
+        assert true;
+    }
+
+    @org.junit.Test
+    public void FailedTest() {
+        assert false;
+    }
+
+}
